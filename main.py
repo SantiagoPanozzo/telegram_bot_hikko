6e9d8f31a34862bdb483fd7c2f22e44364d89e30
SantiagoPanozzo <97817883+SantiagoPanozzo@users.noreply.github.com>
2024-07-01 11:20:46 -0300

    Base

---
+++ main.py
@@ -0,0 +1,47 @@
+import logging, os
+
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from dotenv import load_dotenv
+
+# Cargar variables de entorno (por ahora solo el token del bot)
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+print(TOKEN)
+
+# Habilitar el logger, según la documentación de la API
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hola {user.mention_html()}!",
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("¡Ayuda!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    application = Application.builder().token(TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
